e2847bb5f1ad7523ca73a78f67bf723e9f020c1a
hh-harsh1 <sondagarharsh731@gmail.com>
2024-12-25 13:19:25 +0530

    add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const section = document.querySelector(".section");
 
 async function getRecipe() {
-  const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=cake";
+  const url = "";
   const response = await fetch(url);
   const data = await response.json();
   showfood(data.meals);
