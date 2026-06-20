31e85944d3d809d445baf686db4c26cf9516d295
hh-harsh1 <sondagarharsh731@gmail.com>
2026-06-20 16:52:34 +0530

    adddd extra line

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ function showfood(food) {
     `;
 
     const button = box.querySelector(".getRecipe");
+    console.log("how are you");
 
     button.addEventListener("click", (e) => {
       e.preventDefault;
